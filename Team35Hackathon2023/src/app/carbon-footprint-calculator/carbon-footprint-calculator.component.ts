08f7316096c74815a1bc94dd38e70ae9b869a381
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-07-13 08:54:51 -0400

    carbon footprint done

--- Team35Hackathon2023/src/app/carbon-footprint-calculator/carbon-footprint-calculator.component.ts
+++ Team35Hackathon2023/src/app/carbon-footprint-calculator/carbon-footprint-calculator.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import * as co2Rates from '../../assets/co2Rates.json';
+import * as packageDetails from '../../assets/packageDetails.json';
+import * as transportHistory from '../../assets/transportHistory.json';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,39 +10,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./carbon-footprint-calculator.component.css'],
 })
 export class CarbonFootprintCalculatorComponent implements OnInit {
-  carbonRatesData: any[] = [];
-  transportHistoryData: any[] = [];
-  packageData: any[] = [];
+  carbonRatesData: any[] = co2Rates;
+  transportHistoryData: any[] = transportHistory;
+  packageData: any[] = packageDetails;
   carbonEmissionData: any[] = [];
   map = new Map<string, number>();
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.fetchCarbonRates();
-    this.fetchTransportHistory();
-    this.fetchPackageData();
     this.addCarbonFootprint();
   }
 
-  fetchCarbonRates() {
-    this.http.get<any[]>('assets/co2Rates.json').subscribe((data) => {
-      this.carbonRatesData = data;
-    });
-    console.log(this.carbonRatesData);
-  }
-
-  fetchTransportHistory() {
-    this.http.get<any[]>('assets/transportHistory.json').subscribe((data) => {
-      this.transportHistoryData = data;
-    });
-    console.log(this.transportHistoryData);
-  }
-
-  fetchPackageData() {
-    this.http.get<any[]>('assets/packageDetails.json').subscribe((data) => {
-      this.packageData = data;
-    });
+  addCarbonFootprint() {
     for (let i = 0; i < this.packageData.length; i++) {
       let number = this.packageData[i].WeightPerVolumeForPackage;
       this.map.set(
@@ -48,18 +30,12 @@ export class CarbonFootprintCalculatorComponent implements OnInit {
         parseFloat(number.match(/[0-9.]+/g))
       );
     }
-    console.log(this.packageData);
-  }
-
-  addCarbonFootprint() {
-    console.log(1);
     for (let i = 0; i < this.transportHistoryData.length; i++) {
       var distance = this.transportHistoryData[i].DistanceCoveredInMiles;
       var shippingMethod = this.transportHistoryData[i].MeansOfTransportation;
       var packageWeight = this.map.get(
         this.transportHistoryData[i].TrackingNumber
       )!;
-      console.log(packageWeight);
       var mpg = 0.0;
       var emissionsPerMile = 0.0;
       var maxWeight = 0.0;
@@ -101,8 +77,9 @@ export class CarbonFootprintCalculatorComponent implements OnInit {
       };
       this.carbonEmissionData.push(pack);
     }
-    const data = JSON.stringify(this.carbonEmissionData);
-    console.log(JSON.stringify(this.carbonEmissionData));
+    // const data = JSON.stringify(this.carbonEmissionData);
+    console.log(this.carbonEmissionData);
+    return this.carbonEmissionData;
   }
 }
 
