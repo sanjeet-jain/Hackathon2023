4b8e62aedcde2a0d88239ca683cbf5ec237cfe93
sanjeet-jain <sanjeetjain241210@gmail.com>
2023-07-13 00:52:43 -0400

    added truck animation

--- Team35Hackathon2023/src/app/tracking-detail/tracking-detail.component.ts
+++ Team35Hackathon2023/src/app/tracking-detail/tracking-detail.component.ts
@@ -5,30 +5,42 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-tracking-detail',
   templateUrl: './tracking-detail.component.html',
-  styleUrls: ['./tracking-detail.component.css']
+  styleUrls: ['./tracking-detail.component.css'],
 })
-export class TrackingDetailComponent  implements OnInit{
+export class TrackingDetailComponent implements OnInit {
   cardData: any[] = [];
 
-  trackingNumber: string="";
-  constructor(private http: HttpClient,private route: ActivatedRoute) { }
-
+  trackingNumber: string = '';
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.trackingNumber = this.route.snapshot.paramMap.get('trackingNumber')??"";
-    
+    this.trackingNumber =
+      this.route.snapshot.paramMap.get('trackingNumber') ?? '';
+
     this.fetchCardData(this.trackingNumber);
   }
-  
- 
+  currentIndex = 0;
+  onAnimationEndArr: boolean[] = [];
+
+  onAnimationEnd(currentIndex: number, movingObject: any) {
+    if (currentIndex! < this.onAnimationEndArr.length - 1)
+      movingObject.style.opacity = 0;
+    else {
+      movingObject.style.top = '100%';
+    }
+    this.onAnimationEndArr[currentIndex] = true;
+    console.log(this.onAnimationEndArr[currentIndex - 1]);
+  }
 
   fetchCardData(trackingNumber: string) {
     // Replace 'path/to/your/json' with the actual path to your JSON file
-    this.http.get<any[]>('assets/transportHistory.json').subscribe(data => {
-      console.log(data)
-      this.cardData = data.filter(x=>x.TrackingNumber === trackingNumber);
-      console.log(this.cardData)
-     
+    this.http.get<any[]>('assets/transportHistory.json').subscribe((data) => {
+      console.log(data);
+      this.cardData = data.filter((x) => x.TrackingNumber === trackingNumber);
+      for (let i = 0; i < this.cardData.length; i++) {
+        this.onAnimationEndArr.push(false);
+      }
+      console.log(this.cardData);
     });
   }
 }
